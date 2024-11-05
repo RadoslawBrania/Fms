6f01cdb58353e730f5445e4a11cee22f94360196
RadoslawBrania <RadoslawBrania@DESKTOP-MVCM3V0>
2024-11-05 21:19:55 +0100

    Dodaj pliki projektów.

---
+++ Fms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Fms
+    {
+        public int enchant=0;
+        public int[] chance= new int[9];
+        public int[] cost = new int[9];
+        public long price = 0;
+        bool justburned = false;
+        int bodzio = 350000;
+        Random f = new();
+
+        public Fms()
+        {
+            this.chance[0] = 95;
+            this.chance[1] = 95;
+            this.chance[2] = 95;
+            this.chance[3] = 85;
+            this.chance[4] = 75;
+            this.chance[5] = 65;
+            this.chance[6] = 55;
+            this.chance[7] = 45;
+            this.chance[8] = 35;
+            this.cost[0] = 3000;
+            this.cost[1] = 6000;
+            this.cost[2] = 12000;
+            this.cost[3] = 25000;
+            this.cost[4] = 40000;
+            this.cost[5] = 60000;
+            this.cost[6] = 90000;
+            this.cost[7] = 120000;
+            this.cost[8] = 150000;
+        }
+        public void enhance()
+        {
+
+            this.price += this.cost[this.enchant];
+            if (this.enchant >3 ) this.price += this.bodzio;
+            if (this.f.Next(1, 100) < this.chance[this.enchant])
+            {
+                this.enchant += 1;
+            }
+            else if (this.enchant < 4) { this.enchant = 0; this.price += 2500000; }
+            else if (this.enchant > 0) this.enchant -= 1;
+      
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+
+int bodzio = 280000;
+long price=0;
+
+Console.WriteLine("Hello, World!");
+
+Fms fms = new Fms();
+int i = 0;
+Random f;
+while (i<1000000)
+{
+    fms.enhance();
+    if(fms.enchant == 9)
+    {
+        fms.enchant = 0;
+        i++;
+    }
+    
+}
+
+Console.WriteLine("price per fms is equal to " + (fms.price / 1000000));
